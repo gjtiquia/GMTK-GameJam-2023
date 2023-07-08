b3d4e01f853ea3a3c59eef2147bfe05d698fbcf8
GJ Tiquia <gershom.tiquia@gmail.com>
2023-07-08 20:53:08 +0800

    feat: player satisfaction popup ui

--- Assets/_Project/Scripts/PlayerSatisfaction.cs
+++ Assets/_Project/Scripts/PlayerSatisfaction.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
 
 public enum BossAction
 {
@@ -16,7 +18,8 @@ public enum BossAction
 // (+++) Super Anticipation + Super Attack + Ready to Evade
 // (+) Basic Anticipation + Basic Attack
 
-// (---) Super Attack with no Anticipation
+// (----) Super Attack with no Anticipation
+// (---) Mixed Anticipation + Attack
 // (--) Super Anticipation + Super Attack but not ready to evade 
 // (--) Basic Attack with no Anticipation
 
@@ -25,6 +28,7 @@ public class PlayerSatisfaction : MonoBehaviour
     [Header("General Settings")]
     [SerializeField] private int _initialplayerSatisfaction;
     [SerializeField] private int _maxPlayerSatisfaction;
+    [SerializeField] private float _popupDuration;
 
     [Header("Bonus Settings")]
     [SerializeField] private int _superPairBonus;
@@ -37,6 +41,10 @@ public class PlayerSatisfaction : MonoBehaviour
     [Header("References")]
     [SerializeField] private Hero _hero;
     [SerializeField] private Image _satisfactionBar;
+    [SerializeField] private GameObject _penaltyUI;
+    [SerializeField] private TextMeshProUGUI _penaltyText;
+    [SerializeField] private GameObject _bonusUI;
+    [SerializeField] private TextMeshProUGUI _bonusText;
 
     private int _currentPlayerSatisfaction
     {
@@ -54,12 +62,16 @@ public class PlayerSatisfaction : MonoBehaviour
 
     private Stack<BossAction> _actionStack;
 
+    private Tween _popupTween;
+
     private void Awake()
     {
         _actionStack = new Stack<BossAction>();
         _actionStack.Push(BossAction.NONE);
 
         _currentPlayerSatisfaction = _initialplayerSatisfaction;
+
+        ResetAllPopups();
     }
 
     public void OnSuperAnticipation()
@@ -78,20 +90,57 @@ public class PlayerSatisfaction : MonoBehaviour
         {
             _actionStack.Push(BossAction.SuperAttack);
             _currentPlayerSatisfaction -= _noSuperAnticipationPenalty;
+
+            ShowPenaltyUI("Bruh");
             return;
         }
 
         _actionStack.Pop();
 
         if (_hero.CanDodgeSuperAttack())
+        {
             _currentPlayerSatisfaction += _superPairBonus;
+            ShowBonusUI("Dodged Super Attack");
+        }
         else
+        {
             _currentPlayerSatisfaction -= _tooQuickSuperAttackPenalty;
+            ShowPenaltyUI("Anticipation Too Short");
+        }
     }
 
     private void UpdateUI()
     {
         float normalizedSatisfaction = (float)_currentPlayerSatisfaction / _maxPlayerSatisfaction;
         _satisfactionBar.fillAmount = normalizedSatisfaction;
+    }
+
+    private void ShowPenaltyUI(string text)
+    {
+        ResetAllPopups();
+
+        _penaltyText.text = text;
+        _penaltyUI.SetActive(true);
+
+        _popupTween = DOVirtual.DelayedCall(_popupDuration, () => _penaltyUI.SetActive(false));
+    }
+
+    private void ShowBonusUI(string text)
+    {
+        ResetAllPopups();
+
+        _bonusText.text = text;
+        _bonusUI.SetActive(true);
+
+        _popupTween = DOVirtual.DelayedCall(_popupDuration, () => _bonusUI.SetActive(false));
+    }
+
+    private void ResetAllPopups()
+    {
+        _penaltyUI.SetActive(false);
+        _bonusUI.SetActive(false);
+
+        if (_popupTween != null && _popupTween.IsPlaying())
+            _popupTween.Kill();
     }
 }
