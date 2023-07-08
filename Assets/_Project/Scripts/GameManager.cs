abe42bd9d2f37508dcf32069f55c75ab2b2d1155
GJ Tiquia <gershom.tiquia@gmail.com>
2023-07-08 22:59:53 +0800

    feat: GameInit Scene and complete game flow

--- Assets/_Project/Scripts/GameManager.cs
+++ Assets/_Project/Scripts/GameManager.cs
@@ -5,6 +5,17 @@ public class GameManager : MonoBehaviour
 {
     public void ReloadScene()
     {
+        Scene activeScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(activeScene.name);
+    }
+
+    public void LoadGameScene()
+    {
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void LoadGameInitScene()
+    {
+        SceneManager.LoadScene("GameInitScene");
     }
 }
